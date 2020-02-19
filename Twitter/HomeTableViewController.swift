62c56622488a35bf6c96e5723a3a22a27b9abb6e
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2020-02-19 14:53:51 -0800

    updated Twitter Part 2

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,12 +18,21 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
+        //self.loadTweets()
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+        
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
+    
     @objc func loadTweets(){
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -46,7 +55,6 @@ class HomeTableViewController: UITableViewController {
         })
         
     }
-    
     
     
     
@@ -76,6 +84,9 @@ class HomeTableViewController: UITableViewController {
         if let imageData  = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         
         return cell
     }
