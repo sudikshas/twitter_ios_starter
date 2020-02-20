73c038e400054244bccd24ff6ec318cdde4ff687
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2020-02-19 20:45:15 -0800

    updated Twitter Part 2

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -21,6 +21,7 @@ class TweetCellTableViewCell: UITableViewCell {
     var favorited:Bool = false
     var tweetId:Int = -1
     
+    
     @IBAction func favoriteTweet(_ sender: Any) {
         let tobeFavorited = !favorited
         if (tobeFavorited) {
@@ -40,7 +41,22 @@ class TweetCellTableViewCell: UITableViewCell {
         
     }
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeting: \(error)")
+        })
+    }
+    //var retweeted:Bool  = false
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     
 
